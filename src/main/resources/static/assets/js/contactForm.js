cc6d3727bcad62911e6c1fac8a3acd0c5dfe0fd9
obrad97 <janiobrad@gmail.com>
2021-05-18 16:40:50 +0200

    validation update

--- src/main/resources/static/assets/js/contactForm.js
+++ src/main/resources/static/assets/js/contactForm.js
@@ -1,6 +1,21 @@
-var max = 1000;
-var textArea = $(".polje");
-var wordsLeft = $(".broj-reci");
+$(document).ready(function () {
+  $('#button').click(function (e) {
+    
+    let form = $('#contact-form')
+    if (validation()) {
+      form.attr('method', 'post');
+    }
+    else {
+      e.preventDefault();
+      form.attr('method', '');
+    }
+    
+  });
+});
+
+function recaptchaCallback(){
+  $('#button').removeAttr('disabled');
+};
 
 $(window).resize(function() {
     if( $(this).width() > 900 ) {
@@ -10,6 +25,60 @@ $(window).resize(function() {
   });
 
 function maxWords() {
+    let max = 1000;
+    let textArea = $(".polje");
     let currentWords = textArea.val().length;
+    let wordsLeft = $('.broj-reci');
     wordsLeft.html(max-currentWords);
 }
+
+function validation() {
+  let name = $('#name').val();
+  let email = $('#email').val();
+  let message = $('#message').val();
+  let formIsValid = true;
+  //alert(name +' '+ email +' '+ message);
+  if (name === '') {
+    $('#name').addClass('input-error');
+    $('.name-error').html('<p> Obavezno polje </p>');
+    formIsValid = false;
+  }
+
+  if (name != ''){
+    $('#name').removeClass('input-error');
+    $('.name-error').empty();
+  }
+
+  if (email === '') {
+    $('#email').addClass('input-error');
+    $('.email-error').html('<p> Obavezno polje </p>');
+    formIsValid = false;
+  }
+  if (email != '') {
+    $('#email').removeClass('input-error');
+    $('.email-error').empty();
+    let lastAtPos = email.lastIndexOf('@');
+    let lastDotPos = email.lastIndexOf('.');
+    
+    if (!(lastAtPos < lastDotPos && lastAtPos > 0 && email.indexOf('@@') === -1 && lastDotPos > 2 && (email.length - lastDotPos) > 2)) {
+      $('#email').addClass('input-error');
+      $('.email-error').html('<p> Email nije ispravan </p>');  
+      formIsValid = false;
+    }
+  }
+
+  if (message === '') {
+    $('#message').addClass('input-error');
+    $('.message-error').html('<p> Obavezno polje </p>');
+    formIsValid = false;
+  }
+
+  if (message != ''){
+    $('#message').removeClass('input-error');
+    $('.message-error').empty();
+  }
+
+  return formIsValid;
+}
+
+
